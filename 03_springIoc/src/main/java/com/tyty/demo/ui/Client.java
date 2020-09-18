469c5e11cb318cb5086f16cfbad453bb5cf334b6
zhupeiyu <zhupeiyu@swinfo.com.cn>
2020-09-18 21:34:44 +0800

    spring的Ioc学习

--- 03_springIoc/src/main/java/com/tyty/demo/ui/Client.java
+++ 03_springIoc/src/main/java/com/tyty/demo/ui/Client.java
@@ -15,7 +15,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Client {
     /**
      * 获取spring的Ioc核心容器,并根据id获取对象
-     * @param args
+     *  ApplicationContext的三个常用实现类:
+     *      ClassPathXmlApplicationContext: 它可以加载类路径下的配置文件,要求配置文件必须在类路径下,不在的话,加载不了
+     *      FileSystemXmlApplicationContext: 它可以加载磁盘任意路径下的配置文件(必须有访问权限)
+     *      AnnotationConfigApplication: 它是用于读取注解创建容器的
+     *  核心容器的两个接口引发的问题
+     *   ApplicationContext:
+     *      构建核心容器时,创建对象采用的策略是采用立即加载的方式.也就是说,只要一读取完配置文件马上就创建配置文件中的对象
+     *   BeanFactory:
+     *      他在构建核心容器时,创建对象采取的时延迟加载的方式.也就是说,什么时候根据id获取对象了,什么时候才是真正的创建对象
+     *  @param args
      */
     public static void main(String[] args) {
         // 1.获取核心容器对象
