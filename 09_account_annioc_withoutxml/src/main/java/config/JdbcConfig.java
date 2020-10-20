f1ddcabf7338f631fcb05f6cace0bda52d8c39b6
zhuzhuzhuTA <15265381663@163.com>
2020-10-20 22:27:52 +0800

    今天电脑环境坏了,先随便提交点

--- 09_account_annioc_withoutxml/src/main/java/config/JdbcConfig.java
+++ 09_account_annioc_withoutxml/src/main/java/config/JdbcConfig.java
@@ -6,6 +6,7 @@ package config;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.apache.commons.dbutils.QueryRunner;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -17,6 +18,16 @@ import javax.sql.DataSource;
  */
 //@Configuration 使用@ComponentScan("")扫描包时需要
 public class JdbcConfig {
+
+    @Value("${jdbc.driver}")
+    private String driver;
+    @Value("${jdbc.url}")
+    private String url;
+    @Value("${jdbc.username}")
+    private String username;
+    @Value("${jdbc.password}")
+    private String password;
+
     @Bean(name="runner")
     @Scope("prototype")
     public QueryRunner createQueryRunner(DataSource dataSource){
@@ -31,10 +42,14 @@ public class JdbcConfig {
     public DataSource createDataSource(){
         try{
             ComboPooledDataSource ds = new ComboPooledDataSource();
-            ds.setDriverClass("com.mysql.cj.jdbc.Driver");
-            ds.setJdbcUrl("jdbc:mysql://localhost:3306/springdemo?serverTimezone=UTC ");
-            ds.setUser("root");
-            ds.setPassword("zpy@mysql");
+//            ds.setDriverClass("com.mysql.cj.jdbc.Driver");
+//            ds.setJdbcUrl("jdbc:mysql://localhost:3306/springdemo?serverTimezone=UTC");
+//            ds.setUser("root");
+//            ds.setPassword("zpy@mysql");
+            ds.setDriverClass(driver);
+            ds.setJdbcUrl(url);
+            ds.setUser(username);
+            ds.setPassword(password);
             return ds;
         }catch (Exception e){
             throw new RuntimeException(e);
